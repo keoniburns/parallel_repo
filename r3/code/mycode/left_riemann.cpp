82ac41cf0e71dc83f58d3da0891d0254ca9249bf
keoni Burns <keoni.burns@gmail.com>
2023-10-03 16:17:42 -0700

    update

--- r3/code/mycode/left_riemann.cpp
+++ r3/code/mycode/left_riemann.cpp
@@ -35,7 +35,7 @@ int main() {
     float step = (upper - lower) / n;
     int numBoxes = n / numProcs;
     float start = lower + curRank * step;
-    float end = start + numBoxes * step;
+    float end = start + step;
 
     printf("my_rank=%d, start a=%lf, end b=%lf, number of quadratures = %d, step_size=%lf\n", curRank, start, end,
            numBoxes, step);
