45d407e9b89288e9206f2a4fd4b041a504062cda
keoni Burns <keoni.burns@gmail.com>
2023-10-24 20:09:12 -0700

    update

--- r4/mycode/ex3.c
+++ r4/mycode/ex3.c
@@ -1,237 +1,177 @@
-// Simple MPI program to emulate what the Excel spreadsheet model does for:
-//
-// velocity(t) = Sum[(accel(t)]
-//
-// position(t) = Sum[velocity(t)]
-//
-// Using as many ranks as you wish - should ideally divide into 1800 to avoid possible residual errors.
-//
-// Due to loop carried dependencies, this requires basic dynamic programming to use the final condition of one
-// rank as the initial condition of others at a later time, but adjusted after the computations are done in parallel!
-//
-// This is identical to what's done in the OpenMP version of this code, but information is shared via message passing
-// rather than shared memory.
-//
-// Note: this program will be helpful for the Ex #4 train problem where the anti-derivatives are unknown and all
-// functions must
-//       be integrated numerically.
-//
-// Sam Siewert, 2/24/2023, Cal State Chico - https://sites.google.com/mail.csuchico.edu/sbsiewert/home
-//
-// Please use as you see fit, but please do cite and retain a link to this original source
-// here or my github (https://github.com/sbsiewertcsu/numeric-parallel-starter-code)
-//
-
+/**
+ * @file ex2.c
+ * @author Keoni Burns
+ * @brief implements left Riemann, Trapezoidal, and Simpsons to find the integral of sin
+ * adapted from mpi_trap.4 provided by profssor Sam Siewert
+ * @version 0.1
+ * @date 2023-10-24
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <math.h>
 #include <mpi.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+#include <stdlib.h>
 
-#include "ex4accel.h"
+// #include "ex4accel.h"
+#include "ex4vel.h"
 
-const int MAX_STRING = 100;
-double table_accel(int timeidx);
-double faccel(double time);
-#define STEPS_PER_SEC (10)
+// io and mesasage passing protos
+void Build_mpi_type(double* a_p, double* b_p, int* n_p, MPI_Datatype* input_mpi_t_p);
+void Get_input(int my_rank, int comm_sz, double* a_p, double* b_p, int* n_p);
+
+// integral function protos
+double Trap(double left_endpt, double right_endpt, int trap_count, double base_len);
+double LeftRiemann(double left_endpt, double right_endpt, int rect_count, double base_len);
+double Simpson(double left_endpt, double right_endpt, int rect_count);
+
+// sin
+double f(double x);
 
 int main(void) {
-    char greeting[MAX_STRING];
-    char hostname[MAX_STRING];
-    char nodename[MPI_MAX_PROCESSOR_NAME];
-    int comm_sz;
-    int my_rank, namelen;
+    int my_rank, comm_sz, n, local_n;
+    double a, b, h, local_a, local_b;
+    double local_int, total_int;
 
-    // Parallel summing example
-    double local_sum = 0.0, g_sum = 0.0;
-    double default_sum[sizeof(DefaultProfile) / sizeof(double)];
-    double default_sum_of_sums[sizeof(DefaultProfile) / sizeof(double)];
-    int tablelen = sizeof(DefaultProfile) / sizeof(double);
-    int subrange, residual;
-    double time, dt;
-    int steps = STEPS_PER_SEC;
-    dt = 1.0 / steps;
-}
+    /* Let the system do what it needs to start up MPI */
+    MPI_Init(NULL, NULL);
 
-//     // Fill in default_sum array used to simulate a new table of values, such as
-//     // a velocity table derived by integrating acceleration
-//     //
-//     for (int idx = 0; idx < tablelen; idx++) {
-//         default_sum[idx] = 0.0;
-//         default_sum_of_sums[idx] = 0.0;
-//     }
+    /* Get my process rank */
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-//     printf("Will divide up work for input table of size = %d\n", tablelen);
-//     MPI_Init(NULL, NULL);
-//     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-//     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    /* Find out how many processes are being used */
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
-//     subrange = tablelen / comm_sz;
-//     residual = tablelen % comm_sz;
+    Get_input(my_rank, comm_sz, &a, &b, &n);
 
-//     printf("Went parallel: rank %d of %d doing work %d with residual %d\n", my_rank, comm_sz, subrange,
-//     residual);
+    h = (b - a) / n;       /* h is the same for all processes */
+    local_n = n / comm_sz; /* So is the number of trapezoids  */
 
-//     // START PARALLEL PHASE 1: Sum original DefaultProfile LUT by rank
-//     //
-//     if (my_rank != 0) {
-//         gethostname(hostname, MAX_STRING);
-//         MPI_Get_processor_name(nodename, &namelen);
+    /* Length of each process' interval of
+     * integration = local_n*h.  So my interval
+     * starts at: */
+    local_a = a + my_rank * local_n * h;
+    local_b = local_a + local_n * h;
 
-//         // Now sum up the values in the LUT function
-//         // for (int idx = my_rank * subrange; idx < (my_rank * subrange) + subrange; idx++) {
-//         //     local_sum += DefaultProfile[idx];
-//         //     default_sum[idx] = local_sum;  // Each rank has it's own subset of the data
-//         // }
-//         for (int idx = my_rank * subrange; idx < (my_rank * subrange) + subrange; idx++) {
-//             for (int timeidx = idx; timeidx < steps; timeidx++) {
-//                 time = timeidx + (dt * (double)idx);
-//                 printf("idx: %d, timeidx: %lf\n", idx, timeidx);
-//             }
-//         }
+    printf("simpsons rule\n");
+    local_int = Simpson(local_a, local_b, local_n);
 
-//         // for (int idx = my_rank * subrange; idx <= ((my_rank * subrange) + subrange) * STEPS_PER_SEC; idx++) {
-//         //     int start = my_rank * subrange;
-//         //     time = start + (dt * (double)idx);
-//         //     printf("%015.14lf, %015.14lf\n", time, faccel(time));
-//         // }
+    // printf("left Riemann\n");
+    // local_int = LeftRiemann(local_a, local_b, local_n, h);
 
-//         sprintf(greeting, "Sum of DefaultProfile for rank %d of %d on %s is %lf", my_rank, comm_sz, nodename,
-//                 local_sum);
-//         MPI_Send(greeting, strlen(greeting) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-//     } else {
-//         gethostname(hostname, MAX_STRING);
-//         MPI_Get_processor_name(nodename, &namelen);
+    // printf("trapezoidal riemann\n");
+    // local_int = Trap(local_a, local_b, local_n, h);
 
-//         // Now sum up the values in the LUT function
-//         // for (int idx = 0; idx < subrange; idx++) {
-//         //     local_sum += DefaultProfile[idx];
-//         //     default_sum[idx] = local_sum;  // Each rank has it's own subset of the data
-//         // }
+    /* Add up the integrals calculated by each process */
+    MPI_Reduce(&local_int, &total_int, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-//         for (int idx = 0; idx < subrange; idx++) {
-//             for (int timeidx = idx; timeidx < steps; timeidx++) {
-//                 time = timeidx + (dt * (double)idx);
-//                 printf("idx: %d, timeidx: %lf\n", idx, timeidx);
-//             }
-//         }
+    /* Print the result */
+    if (my_rank == 0) {
+        printf("With n = %d trapezoids, our estimate\n", n);
+        printf("of the integral from %f to %f = %20.15f\n", a, b, total_int);
+    }
 
-//         printf("Sum of DefaultProfile for rank %d of %d on %s is %lf\n", my_rank, comm_sz, nodename, local_sum);
+    /* Shut down MPI */
+    MPI_Finalize();
 
-//         for (int q = 1; q < comm_sz; q++) {
-//             MPI_Recv(greeting, MAX_STRING, MPI_CHAR, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//             printf("%s\n", greeting);
-//         }
-//     }
+    return 0;
+} /*  main  */
 
-//     // This should be the summation of DefaultProfile, which should match the spreadsheet for a train profile for
-//     dt=1.0 MPI_Reduce(&local_sum, &g_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); if (my_rank == 0)
-//     printf("\nRank 0 g_sum = %lf\n", g_sum);
+/**
+ * @brief taken from mpi_trap4.c it builds a data type to pass variables between nodes
+ *
+ */
+void Build_mpi_type(double* a_p, double* b_p, int* n_p, MPI_Datatype* input_mpi_t_p) {
+    int array_of_blocklengths[3] = {1, 1, 1};
+    MPI_Datatype array_of_types[3] = {MPI_DOUBLE, MPI_DOUBLE, MPI_INT};
+    MPI_Aint a_addr, b_addr, n_addr;
+    MPI_Aint array_of_displacements[3] = {0};
 
-//     MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Get_address(a_p, &a_addr);
+    MPI_Get_address(b_p, &b_addr);
+    MPI_Get_address(n_p, &n_addr);
+    array_of_displacements[1] = b_addr - a_addr;
+    array_of_displacements[2] = n_addr - a_addr;
+    MPI_Type_create_struct(3, array_of_blocklengths, array_of_displacements, array_of_types, input_mpi_t_p);
+    MPI_Type_commit(input_mpi_t_p);
+} /* Build_mpi_type */
 
-//     // DISTRIBUTE RESULTS: Now to correct and overwrite all default_sum, update all ranks with full table by
-//     sending
-//     // portion of table from each rank > 0 to rank=0, to fill in missing default data
-//     if (my_rank != 0) {
-//         MPI_Send(&default_sum[my_rank * subrange], subrange, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-//     } else {
-//         for (int q = 1; q < comm_sz; q++) {
-//             MPI_Recv(&default_sum[q * subrange], subrange, MPI_DOUBLE, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+/**
+ * @brief also taken from mpi_trap4.c takes the input from the command line and passes it amongst each node
+ *
+ */
+void Get_input(int my_rank, int comm_sz, double* a_p, double* b_p, int* n_p) {
+    MPI_Datatype input_mpi_t;
+    int rc = 0;
 
-//             // Adjust so initial condition is ending conditon of prior sum - SUPER IMPORTANT to adjust initial
-//             condition
-//             // offset
-//             for (int idx = q * subrange; idx < (q * subrange) + subrange; idx++)
-//                 default_sum[idx] += default_sum[((q - 1) * subrange) + subrange - 1];
-//         }
-//     }
-//     // Make sure all ranks have the full new default table
-//     MPI_Bcast(&default_sum[0], tablelen, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    Build_mpi_type(a_p, b_p, n_p, &input_mpi_t);
 
-//     //
-//     // END PARALLEL PHASE 1: Every rank has the same updated default_sum table now
+    if (my_rank == 0) {
+        printf("Enter a, b, and n\n");
+        rc = scanf("%lf %lf %d", a_p, b_p, n_p);
+        if (rc < 0) perror("Get_input");
+    }
+    MPI_Bcast(a_p, 1, input_mpi_t, 0, MPI_COMM_WORLD);
 
-//     // TRACE: Just a double check on the FIRST MPI_Reduce and trace output of first phase
-//     if (my_rank == 0) {
-// #ifdef SEQ_DEBUG
-//         // Now rank zero has the data from each of the other ranks in one new table
-//         local_sum = 0;
-//         for (int idx = 0; idx < tablelen; idx++) local_sum += default_sum[idx];
-//         printf("Rank %d sum of default_sum = %lf\n", my_rank, local_sum);
-//         for (int idx = 0; idx < tablelen - 1; idx += 100)
-//             printf("t=%d: a=%lf for v=%lf\n", idx, DefaultProfile[idx], default_sum[idx]);
-// #endif
-//         printf("t=%d: a=%lf for v=%lf\n", tablelen - 1, DefaultProfile[tablelen - 1], default_sum[tablelen - 1]);
-//     }
+    MPI_Type_free(&input_mpi_t);
+} /* Get_input */
 
-// START PARALLEL PHASE 2: Now that all ranks have the new default_sum table, we can proceed to sum all of those
-// sums as before
-//
-// Do the next round of summing from the new table
-//
-//     local_sum = 0;
+// uses trapezoidal method to integrate
+double Trap(double left_endpt, double right_endpt, int trap_count, double base_len) {
+    double estimate, x;
+    int i;
 
-//     if (my_rank != 0) {
-//         // Now sum up the values in the new LUT function default_sum
-//         for (int idx = my_rank * subrange; idx < (my_rank * subrange) + subrange; idx++) {
-//             local_sum += default_sum[idx];
-//             default_sum_of_sums[idx] = local_sum;  // Each rank has it's own subset of the data
-//         }
-//     } else {
-//         // Now sum up the values in the new LUT function default_sum
-//         for (int idx = 0; idx < subrange; idx++) {
-//             local_sum += default_sum[idx];
-//             default_sum_of_sums[idx] = local_sum;  // Each rank has it's own subset of the data
-//         }
-//     }
+    estimate = (f(left_endpt) + f(right_endpt)) / 2.0;
+    for (i = 1; i <= trap_count - 1; i++) {
+        x = left_endpt + i * base_len;
+        estimate += f(x);
+    }
+    estimate = estimate * base_len;
 
-//     // This should be the summation of the sums, which should match the spreadsheet for a train profile for
-//     dt=1.0 MPI_Reduce(&local_sum, &g_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); if (my_rank == 0)
-//     printf("\nRank 0 g_sum = %lf\n", g_sum);
+    return estimate;
+} /*  Trap  */
 
-//     // DISTRIBUTE: Finally correct and overwrite all default_sum_of_sums, update all ranks with full table by
-//     sending
-//     // portion of table from each rank > 0 to rank=0, to fill in missing default data
-//     if (my_rank != 0) {
-//         MPI_Send(&default_sum_of_sums[my_rank * subrange], subrange, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-//     } else {
-//         for (int q = 1; q < comm_sz; q++) {
-//             MPI_Recv(&default_sum_of_sums[q * subrange], subrange, MPI_DOUBLE, q, 0, MPI_COMM_WORLD,
-//             MPI_STATUS_IGNORE);
+double LeftRiemann(double left_endpt, double right_endpt, int rect_count, double base_len) {
+    double left_value, x, area = 0.0;
+    int i;
 
-//             // Adjust so initial condition is ending conditon of prior sum - SUPER IMPORTANT to adjust initial
-//             condition
-//             // offset
-//             for (int idx = q * subrange; idx < (q * subrange) + subrange; idx++)
-//                 default_sum_of_sums[idx] += default_sum_of_sums[((q - 1) * subrange) + subrange - 1];
-//         }
-//     }
-//     // Make sure all ranks have the full new default table
-//     MPI_Bcast(&default_sum[0], tablelen, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    // estimate of function on left side to forward integrate
+    x = left_endpt;
+    left_value = f(x);
 
-//     //
-//     // END PHASE 2: Every rank has the same updated default_sum_of_sums table now
+    for (i = 1; i <= rect_count - 1; i++) {
+        // add area of each rectangle to overall area sum
+        area += left_value * base_len;
 
-//     // TRACE: Final double check on the SECOND MPI_Reduce and trace output of second phase
-//     if (my_rank == 0) {
-// #ifdef SEQ_DEBUG
-//         // Now rank zero has the data from each of the other ranks in one new table
-//         local_sum = 0;
-//         for (int idx = 0; idx < tablelen; idx++) local_sum += default_sum_of_sums[idx];
-//         printf("Rank %d sum of default_sum = %lf\n", my_rank, local_sum);
-//         for (int idx = 0; idx < tablelen - 1; idx += 100)
-//             printf("t=%d: a=%lf for v=%lf and p=%lf\n", idx, DefaultProfile[idx], default_sum[idx],
-//                    default_sum_of_sums[idx]);
-// #endif
-//         printf("t=%d: a=%lf for v=%lf and p=%lf\n", tablelen - 2, DefaultProfile[tablelen - 2],
-//                default_sum[tablelen - 2], default_sum_of_sums[tablelen - 2]);
-//     }
+        // advance x by base length for new values to add to area
+        x += base_len;
+        left_value = f(x);
+    }
 
-//     MPI_Finalize();
-//     return 0;
-// }
+    return area;
 
-double table_accel(int timeidx) {
+} /*  LeftRiemann  */
+
+double Simpson(double left_endpt, double right_endpt, int rect_count) {
+    double h = (right_endpt - left_endpt) / rect_count;  // calculate the height
+    double sum = f(left_endpt) + f(right_endpt);         // integrate
+
+    for (int i = 1; i < rect_count; i++) {
+        double x = left_endpt + i * h;
+        // if even multiply by 2 otherwise multiply by 4
+        if (i % 2 == 0) {
+            sum += 2 * f(x);
+        } else {
+            sum += 4 * f(x);
+        }
+    }
+
+    return (h / 3) * sum;
+} /*  Simpsons  */
+
+// table look-up for function profile given and velocity profile determined
+double table_function(int timeidx) {
     long unsigned int tsize = sizeof(DefaultProfile) / sizeof(double);
 
     // Check array bounds for look-up table
@@ -243,12 +183,13 @@ double table_accel(int timeidx) {
     return DefaultProfile[timeidx];
 }
 
-double faccel(double time) {
+double table_interp(double time) {
     int timeidx = (int)time;
-
     int timeidx_next = ((int)time) + 1;
-
     double delta_t = time - (double)((int)time);
 
-    return (table_accel(timeidx) + ((table_accel(timeidx_next) - table_accel(timeidx)) * delta_t));
+    return (table_function(timeidx) + ((table_function(timeidx_next) - table_function(timeidx)) * delta_t));
 }
+
+// x is the time
+double f(double x) { return (table_interp(x)); } /* f */
