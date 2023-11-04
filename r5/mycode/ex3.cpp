e0c035d039d97a24b71a187ea9459cd9773ed447
keoni Burns <keoni.burns@gmail.com>
2023-11-03 17:59:36 -0700

    update

--- r5/mycode/ex3.cpp
+++ r5/mycode/ex3.cpp
@@ -151,7 +151,7 @@ void multiplication(matrix_data A, matrix_data B, matrix_data &C) {
     C.m = B.m;
     C.matrix.resize(C.n, vector<double>(C.m));
 
-#pragma omp parallel for num_threads(threads) collapse(3)
+    // #pragma omp parallel for num_threads(threads) collapse(3)
     for (int i = 0; i < A.n; i++) {
         for (int j = 0; j < B.m; j++) {
             for (int k = 0; k < A.m; k++) {
