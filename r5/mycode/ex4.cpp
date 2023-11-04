bf7c151b492f612137298378683d4cc31078e557
keoni Burns <keoni.burns@gmail.com>
2023-11-04 15:49:12 -0700

    update

--- r5/mycode/ex4.cpp
+++ r5/mycode/ex4.cpp
@@ -163,7 +163,7 @@ int forwardStep(matrix_data &A) {
         maxPos = k;                    // init current max
         maxVal = A.matrix[maxPos][k];  // grab value
 
-#pragma omp for
+#pragma omp for num_threadss(THREADS)
         for (int i = k + 1; i < A.n; i++) {  // search rows for the largest val and pivot
             if (abs(A.matrix[i][k]) > maxVal) {
                 maxVal = A.matrix[i][k];
@@ -181,7 +181,8 @@ int forwardStep(matrix_data &A) {
                     rowSwap(A, k, maxPos);
                 }
             }
-#pragma omp for
+
+#pragma omp for num_threads(THREADS)
             for (int i = k + 1; i < A.n; i++) {
                 double reduce = A.matrix[i][k] / A.matrix[k][k];
                 for (int j = k + 1; j <= A.n; j++) {
