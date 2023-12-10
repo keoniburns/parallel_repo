56c07195bdc95ffded3f542c1dcfde8bb16ba9d9
keoni Burns <keoni.burns@gmail.com>
2023-12-09 17:31:13 -0800

    yeuh

--- final/mpi_pshift.cpp
+++ final/mpi_pshift.cpp
@@ -210,15 +210,15 @@ void smbPitchShift(double pitchShift, long numSampsToProcess, long fftFrameSize,
         gInit = true;
     }
 
-/* main processing loop */
-/**
- * @brief i believe this is the main split for mpi where we will take the total number of samples
- * and divide it up by n workers and i think it'll work
- */
-// #pragma omp parallel for num_threads(NUM_THREADS)
-#pragma omp parallel for num_threads(NUM_THREADS)                                                                \
-    shared(gInFIFO, gOutFIFO, gRover, gFFTworksp, gLastPhase, gAnaMagn, gAnaFreq, gSynMagn, gSynFreq, gSumPhase, \
-               gOutputAccum) private(i, k, window, real, imag, magn, phase, tmp, qpd, index)
+    /* main processing loop */
+    /**
+     * @brief i believe this is the main split for mpi where we will take the total number of samples
+     * and divide it up by n workers and i think it'll work
+     */
+    // #pragma omp parallel for num_threads(NUM_THREADS)
+    /*#pragma omp parallel for num_threads(NUM_THREADS)                                                                \
+        shared(gInFIFO, gOutFIFO, gRover, gFFTworksp, gLastPhase, gAnaMagn, gAnaFreq, gSynMagn, gSynFreq, gSumPhase, \
+                   gOutputAccum) private(i, k, window, real, imag, magn, phase, tmp, qpd, index) */
     for (i = 0; i < numSampsToProcess; i++) {
         /* As long as we have not yet collected enough data just read in */
         gInFIFO[gRover] = indata[i];
