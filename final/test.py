a832b88f0f4827aef951e717262a51dc79ef5e4c
keoni Burns <keoni.burns@gmail.com>
2023-12-14 16:37:57 -0800

    yeuh

--- final/test.py
+++ final/test.py
@@ -26,15 +26,16 @@ for i in range(2, 9, 2):  # Incrementing by 2 from 2 to 8
     for j in range(2, 4, 1):
         command1 = [
             "mpirun -n " + str(j) + " -ppn 8 ./" + program1,
+            "sounds/" + infile1,
+            "sounds/" + outfile1 + str(i) + str(j),
             str(i),
-            infile1 + str(i),
-            outfile1 + str(i),
         ]
 
     # Run the programs using subprocess
     result1 = subprocess.run(command1, stdout=subprocess.PIPE, text=True)
     program1_outputs.append(result1.stdout)
 
+command2 = ["./" + program1, "sounds/" + infile1, "sounds/" + outfile1]
 result2 = subprocess.run(command2, stdout=subprocess.PIPE, text=True)
 
 for i, output in enumerate(program1_outputs, start=2):
