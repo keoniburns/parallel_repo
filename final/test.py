daaf66bd11872a2bbdbecd77737d203bca88b22a
keoni Burns <keoni.burns@gmail.com>
2023-12-14 18:00:31 -0800

    yeuh

--- final/test.py
+++ final/test.py
@@ -12,9 +12,10 @@ program1_outputs = []
 
 
 # Loop to run the programs with different input and output files
-for i in range(2, 9, 2):  # Incrementing by 2 from 2 to 8
-    # Build the command lines with different input and output files
-    for j in range(2, 5, 1):
+print("parallel results")
+for j in range(2, 5, 1):
+    for i in range(2, 9, 2):  # Incrementing by 2 from 2 to 8
+        # Build the command lines with different input and output files
         # command1 = [
         #     "mpirun -n " + str(j) + " -ppn 8 ./mpishift",
         #     "sounds/" + infile,
@@ -32,10 +33,12 @@ for i in range(2, 9, 2):  # Incrementing by 2 from 2 to 8
             "auotest/" + str(i) + str(j) + outfile1,
             str(i),
         ]
+        result1 = subprocess.run(command1, stdout=subprocess.PIPE, text=True)
+        print("threads: " + str(i), "workers: " + str(j), result1)
 
         # Run the programs using subprocess
-        result1 = subprocess.run(command1, stdout=subprocess.PIPE, text=True)
-        program1_outputs.append(result1.stdout)
+
+        # program1_outputs.append(result1.stdout)
 
 command2 = ["./smb", "sounds/" + infile, "autotest/" + outfile1]
 result2 = subprocess.run(command2, stdout=subprocess.PIPE, text=True)
